66260cd7fd60b082a623edf7c024c97a97d09c22
PierreSQS <nomo_messina@web.de>
2021-04-05 06:48:43 +0200

    Exercise 4 NOK: Solution with @Qualifier Annotation

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
  */
 @Profile({"dog", "default"})
 @Service
-@Primary
 public class DogPetService  implements  PetService{
     @Override
     public String getPetType(){
